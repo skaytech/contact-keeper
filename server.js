822d196421ffc8831ad788f2891bd7853f15878c
skay <skaytech30@gmail.com>
2020-07-24 19:24:04 +0530

    Fixed server.js & added meta description text

--- server.js
+++ server.js
@@ -17,9 +17,11 @@ app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
 // Check for Production env
-if ((process.env.NODE_ENV = 'production')) {
+if (process.env.NODE_ENV === 'production') {
   //Set Static Folder Path
-  app.use('*', (req, res) =>
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
